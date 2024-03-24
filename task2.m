92f951f7cadcce95dcffff96a8003ef8218935c0
Oleksandr <vasa22292@gmail.com>
2024-03-24 16:41:02 +0100

    fix(task2): refacor

--- task2.m
+++ task2.m
@@ -1,24 +1,21 @@
-% Define the range of x values
 x = logspace(-2, 2, 1000);
-
-
 y = (log(x)./x.^2 )-(x.^3);
 
-Tx = abs((1 - 2*log(x) - 3*x.^5) ./ (log(x) - x.^5));
-max_abs_error = y .* Tx .* eps(single(1)) / 2; 
+% Maximum error
+Tx = (1 - 2*log(x) - 3*x.^5) ./ (log(x) - x.^5);
+max_abs_error = abs(y .* Tx .* double(eps(single(1))) / 2); 
 max_rel_error = abs(max_abs_error ./ y) ;
 
-
+% Real error
 ys = (log(double(single(x)))./double(single(x)).^2 )-(double(single(x)).^3);
 abs_error = abs(y - ys);
 rel_error = abs(abs_error ./ y);
 
 
-% Plot the function
-loglog(x, rel_error, 'r');
+% Plot errors
+plot(x, rel_error, 'Color', 'red', 'LineWidth', 1.5);
 hold on;
-loglog(x, max_rel_error, 'b');
-xlabel('x');
-ylabel('y');
-title('HUI');
-grid on;
+plot(x, max_rel_error, 'Color', 'blue', 'LineWidth',1.5);
+xscale('log')
+yscale('log')
+legend('real error', 'maximum error'), xlabel('x'), ylabel('σ'), title('Task 2')
