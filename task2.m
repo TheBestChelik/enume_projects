32b90b2abcd04039c02e395b62da718300329e5b
Oleksandr <vasa22292@gmail.com>
2024-04-12 09:25:56 +0200

    refactor: task2

--- task2.m
+++ task2.m
@@ -1,21 +1,26 @@
+clc, clearvars, close all
+
+epsilon  = double(eps("single")/2);
 x = logspace(-2, 2, 1000);
-y = (log(x)./x.^2 )-(x.^3);
+y_double = (log(x) ./ x.^2 )-(x.^3);
 
-% Maximum error
+% Calculation maximum possible absolute and relative errors
 Tx = (1 - 2*log(x) - 3*x.^5) ./ (log(x) - x.^5);
-max_abs_error = abs(y .* Tx .* double(eps(single(1))) / 2); 
-max_rel_error = abs(max_abs_error ./ y) ;
+max_delta = abs(y_double .* Tx .* epsilon); 
+max_sigma = abs(max_delta ./ y_double) ;
 
-% Real error
-ys = (log(double(single(x)))./double(single(x)).^2 )-(double(single(x)).^3);
-abs_error = abs(y - ys);
-rel_error = abs(abs_error ./ y);
+% Calculate true error, due to uncertainty  of x
+y_single = (log(double(single(x)))./double(single(x)).^2 )-(double(single(x)).^3);
+delta = abs(y_double - y_single);
+sigma = abs(delta ./ y_double);
 
 
-% Plot errors
-plot(x, rel_error, 'Color', 'red', 'LineWidth', 1.5);
-hold on;
-plot(x, max_rel_error, 'Color', 'blue', 'LineWidth',1.5);
+% Plot the results
+figure(1)
+hold on
+plot(x, sigma, 'Color', 'red', 'LineWidth', 1.5);
+plot(x, max_sigma, 'Color', 'blue', 'LineWidth',1.5);
 xscale('log')
 yscale('log')
-legend('real error', 'maximum error'), xlabel('x'), ylabel('σ'), title('Task 2')
+legend('|σ(ỹ)|', 'max(|σ(ỹ)|)'), xlabel('x'), ylabel('σ'), title('Task 2')
+hold off
